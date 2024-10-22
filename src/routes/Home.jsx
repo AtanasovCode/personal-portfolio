48b18334cb0e4f9c861672536ec3952b4e774ae3
AtanasovCode <a_ljupce@yahoo.com>
2024-10-22 17:25:39 +0200

    installed Zustand & created a Zustand store

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,9 +1,13 @@
-
+import { usePortfolioStore } from "../../usePortfolioStore";
 
 const Home = () => {
+
+    const { bears } = usePortfolioStore();
+
     return (
-        <div className="bg-blue-900">
+        <div className="bg-blue-900 text-white">
             <div className="text-white font-bold text-xl">Homepage</div>
+            {bears}
         </div>
     );
 }
